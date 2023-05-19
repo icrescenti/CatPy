a9aa1e32e7ec88cb5bc2ebb424aafa46b7cf8f01
icrescenti <icrescenti77@gmail.com>
2023-05-19 11:15:52 +0200

    fibonacci en català

--- exemples/fibonacci.py
+++ exemples/fibonacci.py
@@ -1,17 +1,17 @@
-def fibonacci(n):
-    if n <= 0:
-        return []
-    elif n == 1:
-        return [0]
-    elif n == 2:
-        return [0, 1]
-    else:
+funció fibonacci(n):
+    en_cas_que n <= 0:
+        retorna []
+    en_cas_contrari_si n == 1:
+        retorna [0]
+    en_cas_contrari_si n == 2:
+        retorna [0, 1]
+    del_contrari:
         seqüència = [0, 1]
-        while len(seqüència) < n:
+        mentres lallargaria_de(seqüència) < n:
             next_number = seqüència[-1] + seqüència[-2]
-            seqüència.append(next_number)
-        return seqüència
+            seqüència.concatena(next_number)
+        retorna seqüència
 
 n = 10
 seqüència = fibonacci(n)
-print(seqüència)
+imprimir(seqüència)
